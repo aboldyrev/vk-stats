fa7fedcf67258c26381dd894286aa20fab9cfcdf
aboldyrev <andreybold@bk.ru>
2019-11-09 13:58:31 +0300

    Добавлена заготовка для команды сбора данных

---
+++ app/Console/Commands/VkDataCollectionCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+
+
+use Illuminate\Console\Command;
+
+class VkDataCollectionCommand extends Command
+{
+	protected $name = 'vk:data-collection';
+
+	protected $description = 'Запуск сбора данных';
+
+
+	public function handle() {
+		//
+	}
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,28 +3,34 @@
 namespace App\Console;
 
 use App\Console\Commands\RouteListCommand;
+use App\Console\Commands\VkDataCollectionCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        RouteListCommand::class,
-    ];
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		RouteListCommand::class,
+		VkDataCollectionCommand::class,
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        //
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+	 *
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule) {
+		$schedule
+			->command(VkDataCollectionCommand::class)
+			->everyMinute()
+			->name('vk-data-collection')
+			->withoutOverlapping();
+	}
 }
