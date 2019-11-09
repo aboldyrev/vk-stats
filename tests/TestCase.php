7b03b92674cb3191b8639b8ef8b038d0627fb5af
aboldyrev <andreybold@bk.ru>
2019-11-09 16:43:41 +0300

    В TestCase добавлена генерация токена доступа

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Artisan;
 use Laravel\Passport\Passport;
 use Spatie\Permission\Models\Permission;
 
 /**
  * Class TestCase
+ *
+ * @property array $header
  */
 abstract class TestCase extends Laravel\Lumen\Testing\TestCase
 {
@@ -27,5 +30,17 @@ abstract class TestCase extends Laravel\Lumen\Testing\TestCase
 		Artisan::call('db:seed');
 
 		Passport::tokensCan(Permission::pluck('id', 'name')->toArray());
+
+		/** @var User $user */
+		$user = User::find(1);
+
+		$permissions = $user->permissions()->get();
+		$array_permissions = $permissions->pluck('name')->toArray();
+
+		$token = $user
+			->createToken('token', $array_permissions)
+			->accessToken;
+
+		$this->header = [ 'HTTP_AUTHORIZATION' => 'Bearer ' . $token ];
 	}
 }
